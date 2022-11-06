3e9916a8010c27b06c02dd5033fd2f70488f623d
VictorVidal08 <victorvidal08@hotmail.com>
2022-11-06 15:21:20 -0300

    feat: crating productList and details pages structure

--- src/components/detalhesProduto/detalhes.jsx
+++ src/components/detalhesProduto/detalhes.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
 function Detalhes() {
+    const fakeProduct = [
+        {
+          "_id": "6366ae6d46ec69aa5de97dd8",
+          "produto": "celular",
+          "valor": 2200,
+          "descricao": "o top do momento",
+          "created": "1970-01-01T00:00:00.000Z",
+          "updated": "1970-01-01T00:00:00.000Z"
+        }]
   return (
     <section>
       <h2>Detalhes</h2>
+      { fakeProduct.map((product) => (
+        <div>
+            <h3>{ product.produto }</h3>
+            <p>{ `Valor R$: ${ product.valor }` }</p>
+            <p>{  product.descricao }</p>
+            <button>
+                Editar
+            </button>
+        </div>
+      )) }
     </section>
   );
 }
--- src/components/listaProdutos/listaDeProdutos.jsx
+++ src/components/listaProdutos/listaDeProdutos.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { getProducts } from '../../service/requests';
 
 function ListaDeProdutos() {
+
+    const history = useHistory();
+    
     useEffect(() => {
         const fetchProducts = async () => {
           const { data } = await getProducts();
@@ -10,9 +14,61 @@ function ListaDeProdutos() {
         };
         fetchProducts();
       }, []);
+
+const fakeProducts = [
+    {
+      "_id": "6366ae6d46ec69aa5de97dd8",
+      "produto": "celular",
+      "valor": 2200,
+      "descricao": "o top do momento",
+      "created": "1970-01-01T00:00:00.000Z",
+      "updated": "1970-01-01T00:00:00.000Z"
+    },
+    {
+      "_id": "6366ae6d46ec69aa5de97dd9",
+      "produto": "esteira",
+      "valor": 4000,
+      "descricao": "corre sozinha",
+      "created": "1970-01-01T00:00:00.000Z",
+      "updated": "1970-01-01T00:00:00.000Z"
+    },
+    {
+      "_id": "6366ae6d46ec69aa5de97dda",
+      "produto": "alexa",
+      "valor": 500,
+      "descricao": "companheira",
+      "created": "1970-01-01T00:00:00.000Z",
+      "updated": "1970-01-01T00:00:00.000Z"
+    },
+    {
+      "_id": "6366ae6d46ec69aa5de97ddb",
+      "produto": "halter 1200kg",
+      "valor": 1200,
+      "descricao": "pesado demais",
+      "created": "1970-01-01T00:00:00.000Z",
+      "updated": "1970-01-01T00:00:00.000Z"
+    }
+  ]
+
   return (
     <section>
       <h2>ListaDeProdutos</h2>
+      { fakeProducts.map((product) => (
+        <div>
+            <p>{ product.produto }</p>
+            <p>Eletrônico</p>
+            {/* { categoria do produto não foi informada no README do backend } */}
+            <p>{ `R$: ${ product.valor }` }</p>
+            <button
+                id={ product._id }
+                onClick={ () => {
+                    history.push('/produtos/detalhes');
+                  } }
+            >
+                Detalhes
+            </button>
+        </div>
+      )) }
     </section>
   );
 }
