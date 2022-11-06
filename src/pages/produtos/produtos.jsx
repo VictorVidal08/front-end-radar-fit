58d8257110bd88c8df3692b57e8b98c0cea2974d
VictorVidal08 <victorvidal08@hotmail.com>
2022-11-06 13:46:48 -0300

    feat: adding request file and novo produto button

--- src/pages/produtos/produtos.jsx
+++ src/pages/produtos/produtos.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Detalhes from '../../components/detalhesProduto/detalhes';
 import ListaDeProdutos from '../../components/listaProdutos/listaDeProdutos';
+import './produtos.css';
 
 function Produtos() {
   return (
     <section>
-      <h1>TESTE</h1>
+      <h1>Produtos</h1>
+      <button
+        class="icon-btn add-btn">
+        <div class="add-icon"></div>
+        <div class="btn-txt">Novo Produto</div>
+      </button>
       <ListaDeProdutos />
       <Detalhes />
     </section>
