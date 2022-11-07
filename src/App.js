aefa3a745d6025825cb8538ac50d375a36d75b95
VictorVidal08 <victorvidal08@hotmail.com>
2022-11-06 22:26:33 -0300

    feat: removing unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Produtos from './pages/produtos/produtos';
 import NovoProduto from './components/novoProduto/novoProduto';
